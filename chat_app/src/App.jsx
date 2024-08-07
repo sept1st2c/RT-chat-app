3a89518bdb8cc48e6f8d995cfef1ca39434393c1
sept1st2c <sept1st2c@gmail.com>
2024-08-07 22:46:31 +0530

    fuck it we ball

--- chat_app/src/App.jsx
+++ chat_app/src/App.jsx
@@ -1,14 +1,55 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+import { useEffect, useState } from "react";
 import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
+const pkmnTrainers = ["Ash", "Gary", "Red", "Misty"];
 
+function App() {
+  // const [count, setCount] = useState(0);
+  const [username] = useState(
+    pkmnTrainers[Math.floor(Math.random() * pkmnTrainers.length)]
+  );
+  const [message, setMessage] = useState("");
+  const [chat, setChat] = useState([]);
+  const [typing, setTyping] = useState("");
+  const [dotCount, setDotCount] = useState(1);
+
+  useEffect(() => {
+    console.log(username);
+
+    return () => {};
+  });
+
+  function writeMessage(e) {
+    const currMessage = e.target.value;
+    setMessage(currMessage);
+    // console.log(currMessage)
+  }
+  function handleSubmit(e) {
+    e.preventDefault();
+    setChat((prevChat) => [
+      ...prevChat,
+      { user: "you", message: message, dir: "right" },
+    ]);
+  }
   return (
     <>
       <h1>Chat app</h1>
+      <div className="message-container">
+        <div className="message right">you joined the chat</div>
+        <div className="message left">Mimic Tear joined the chat</div>
+      </div>
+      <div className="send-container">
+        <input
+          type="text"
+          placeholder="Enter message"
+          value={message}
+          onChange={writeMessage}
+        />
+        <button onClick={handleSubmit}>Send</button>
+      </div>
     </>
   );
 }
